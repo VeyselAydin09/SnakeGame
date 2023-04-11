d52700ca846133ccc977ec11e160b214ba606c1c
Veysel Aydin <weycell.aydin09@gmail.com>
2023-04-11 21:24:43 +0300

    function drow

--- snake.js
+++ snake.js
@@ -14,3 +14,16 @@ export function update() {
   snakeBody[0].x += inputDirection.x;
   snakeBody[0].y += inputDirection.y;
 }
+export function draw(gameBoard) {
+  snakeBody.forEach((segment) => {
+    const snakeElement = document.createElement("div");
+    snakeElement.style.gridRowStart = segment.y;
+    snakeElement.style.gridColumnStart = segment.x;
+    snakeElement.classList.add("snake");
+    gameBoard.appendChild(snakeElement);
+  });
+}
+
+export function expandSnake(amount) {
+  newSegments += amount;
+}
