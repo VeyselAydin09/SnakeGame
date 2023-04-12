f1a8e232273f2b7007d43d7ef7cd40f1009f013b
Veysel Aydin <weycell.aydin09@gmail.com>
2023-04-12 12:33:16 +0300

    function update

--- food.js
+++ food.js
@@ -1,2 +1,11 @@
 import { onSnake, expandSnake } from "./snake.js";
 import { randomGridPosition } from "./grid.js";
+let food = getRandomFoodPosition();
+const EXPANSION_RATE = 1;
+
+export function update() {
+  if (onSnake(food)) {
+    expandSnake(EXPANSION_RATE);
+    food = getRandomFoodPosition();
+  }
+}
