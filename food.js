623e95eb50749760e46bf77022c0fa877007af09
Veysel Aydin <weycell.aydin09@gmail.com>
2023-04-13 14:16:13 +0300

    getRandomFoodPosition

--- food.js
+++ food.js
@@ -16,3 +16,11 @@ export function draw(gameBoard) {
   foodElement.classList.add("food");
   gameBoard.appendChild(foodElement);
 }
+
+function getRandomFoodPosition() {
+  let newFoodPosition;
+  while (newFoodPosition == null || onSnake(newFoodPosition)) {
+    newFoodPosition = randomGridPosition();
+  }
+  return newFoodPosition;
+}
