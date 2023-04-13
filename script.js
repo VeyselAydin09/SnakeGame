0c6cb46e8bb37e80f0128d809938542b307a0346
Veysel Aydin <weycell.aydin09@gmail.com>
2023-04-13 14:19:15 +0300

    continue

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ import {
   getSnakeHead,
   snakeIntersection,
 } from "./snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js";
+import { outsideGrid } from "./grid.js";
+
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById("game-board");
